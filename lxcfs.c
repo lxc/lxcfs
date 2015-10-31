977ac879c1c8b0b3f29e7b10bc70682c5036200b
Serge Hallyn <serge.hallyn@ubuntu.com>
2015-10-30 23:33:16 -0500

    drop support for precise's glib
    since we can't run in travis anyway
    
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- lxcfs.c
+++ lxcfs.c
@@ -25,8 +25,10 @@
 #include <sys/mount.h>
 #include <wait.h>
 
+#ifdef FORTRAVIS
 #define GLIB_DISABLE_DEPRECATION_WARNINGS
 #include <glib-object.h>
+#endif
 
 #include "cgmanager.h"
 #include "config.h" // for VERSION
@@ -3008,9 +3010,11 @@ int main(int argc, char *argv[])
 	int nargs = 5, cnt = 0;
 	char *newargv[6];
 
+#ifdef FORTRAVIS
 	/* for travis which runs on 12.04 */
 	if (glib_check_version (2, 36, 0) != NULL)
 		g_type_init ();
+#endif
 
 	/* accomodate older init scripts */
 	swallow_arg(&argc, argv, "-s");
