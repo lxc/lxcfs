341e407e24d45d5fa854bdd6abaa5a4e34a35bc9
Serge Hallyn <serge.hallyn@ubuntu.com>
2015-10-30 21:15:26 -0500

    use LXCFSDIR in meminfo hierarchy test
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- tests/test_meminfo_hierarchy.sh
+++ tests/test_meminfo_hierarchy.sh
@@ -25,10 +25,10 @@ trap cleanup EXIT HUP INT TERM
 cgm create memory ${cg1}
 cgm setvalue memory ${cg1} memory.limit_in_bytes 500000000
 cgm movepid memory ${cg1} $$
-m1=`awk '/^MemTotal:/ { print $2 }' /var/lib/lxcfs/proc/meminfo`
+m1=`awk '/^MemTotal:/ { print $2 }' ${LXCFSDIR}/proc/meminfo`
 cgm create memory ${cg2}
 cgm movepid memory ${cg2} $$
-m2=`awk '/^MemTotal:/ { print $2 }' /var/lib/lxcfs/proc/meminfo`
+m2=`awk '/^MemTotal:/ { print $2 }' ${LXCFSDIR}/proc/meminfo`
 [ $m1 -eq $m2 ]
 
 FAILED=0
