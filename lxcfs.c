257f639baeab38f607ebb10db7ea6da0e5eba777
Wolfgang Bumiller <w.bumiller@proxmox.com>
2016-01-07 12:59:52 +0100

    avoid strlen in must_strcat_pid
    sprintf() already returns the length
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- lxcfs.c
+++ lxcfs.c
@@ -77,7 +77,7 @@ static void must_strcat_pid(char **src, size_t *sz, size_t *asz, pid_t pid)
 	char *d = *src;
 	char tmp[30];
 
-	sprintf(tmp, "%d\n", (int)pid);
+	int tmplen = sprintf(tmp, "%d\n", (int)pid);
 
 	if (!d) {
 		do {
@@ -85,15 +85,15 @@ static void must_strcat_pid(char **src, size_t *sz, size_t *asz, pid_t pid)
 		} while (!d);
 		*src = d;
 		*asz = BUF_RESERVE_SIZE;
-	} else if (strlen(tmp) + *sz + 1 >= *asz) {
+	} else if (tmplen + *sz + 1 >= *asz) {
 		do {
 			d = realloc(d, *asz + BUF_RESERVE_SIZE);
 		} while (!d);
 		*src = d;
 		*asz += BUF_RESERVE_SIZE;
 	}
-	memcpy(d+*sz, tmp, strlen(tmp));
-	*sz += strlen(tmp);
+	memcpy(d+*sz, tmp, tmplen);
+	*sz += tmplen;
 	d[*sz] = '\0';
 }
 
