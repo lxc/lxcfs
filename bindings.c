bbb508dd81b30eba95466564c9c36042e840115c
Christian Brauner <christian.brauner@ubuntu.com>
2018-08-17 17:33:22 +0200

    bindings: prevent double free
    Closes #252.
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- bindings.c
+++ bindings.c
@@ -2054,6 +2054,7 @@ static void do_release_file_info(struct fuse_file_info *fi)
 	free(f->buf);
 	f->buf = NULL;
 	free(f);
+	f = NULL;
 }
 
 int cg_releasedir(const char *path, struct fuse_file_info *fi)
