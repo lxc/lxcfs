d4447d1555dcc4dd915607505c314a3925f3dc69
tomponline <tomp@tomp.uk>
2019-01-22 00:18:26 +0000

    Revert "bindings: protect do_release_file_info() with mutex"
    This reverts commit 0108dc1cc6eab76392d228ee4ca844729096473d.
    
    I am still not happy with this, as have experienced some further hangs in lxcfs with this patch.
    Best to revert it whilst I investigate further.
    
    Signed-off-by: tomponline <tomp@tomp.uk>

--- bindings.c
+++ bindings.c
@@ -2169,16 +2169,11 @@ out:
 	return ret;
 }
 
-static pthread_mutex_t do_release_file_info_lock = PTHREAD_MUTEX_INITIALIZER;
-
 static void do_release_file_info(struct fuse_file_info *fi)
 {
-	lock_mutex(&do_release_file_info_lock);
-
 	struct file_info *f = (struct file_info *)fi->fh;
 
 	if (!f)
-		unlock_mutex(&do_release_file_info_lock);
 		return;
 
 	fi->fh = 0;
@@ -2193,8 +2188,6 @@ static void do_release_file_info(struct fuse_file_info *fi)
 	f->buf = NULL;
 	free(f);
 	f = NULL;
-
-	unlock_mutex(&do_release_file_info_lock);
 }
 
 int cg_releasedir(const char *path, struct fuse_file_info *fi)
