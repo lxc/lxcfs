ce8fc84c7ffb790e858233455793ca06f9722966
Christian Brauner <christian.brauner@ubuntu.com>
2020-02-27 15:23:02 +0100

    api_extensions: add and log api extensions
    New features should get an entry in here:
    
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: mount namespace: 4
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: hierarchies:
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:   0: fd:   5:
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:   1: fd:   6: name=systemd
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:   2: fd:   7: cpu,cpuacct
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:   3: fd:   8: rdma
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:   4: fd:   9: devices
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:   5: fd:  10: pids
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:   6: fd:  11: net_cls,net_prio
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:   7: fd:  12: memory
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:   8: fd:  13: perf_event
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:   9: fd:  14: blkio
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:  10: fd:  15: freezer
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:  11: fd:  16: cpuset
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]:  12: fd:  17: hugetlb
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: Kernel supports pidfds
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: api_extensions:
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - cgroups
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - sys_cpu_online
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - proc_cpuinfo
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - proc_diskstats
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - proc_loadavg
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - proc_meminfo
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - proc_stat
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - proc_swaps
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - proc_uptime
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - shared_pidns
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - cpuview_daemon
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - loadavg_daemon
    Feb 27 15:21:54 wittgenstein lxcfs[2129877]: - pidfds
    
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

---
+++ api_extensions.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: LGPL-2.1+ */
+
+#ifndef __LXCFS_API_EXTENSIONS_H
+#define __LXCFS_API_EXTENSIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "config.h"
+
+/*
+ * api_extensions is the list of all API extensions in the order they were
+ * added.
+ */
+static char *api_extensions[] = {
+	"cgroups",
+	"sys_cpu_online",
+	"proc_cpuinfo",
+	"proc_diskstats",
+	"proc_loadavg",
+	"proc_meminfo",
+	"proc_stat",
+	"proc_swaps",
+	"proc_uptime",
+	"shared_pidns",
+	"cpuview_daemon",
+	"loadavg_daemon",
+	"pidfds",
+};
+
+static size_t nr_api_extensions = sizeof(api_extensions) / sizeof(*api_extensions);
+
+#endif /* __LXCFS_API_EXTENSIONS_H */
