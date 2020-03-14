426e9c07d1385e2d8880433f8cc491e62adb40cf
Christian Brauner <christian.brauner@ubuntu.com>
2020-03-14 21:27:02 +0100

    test_sigusr2: improve tests
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- tests/test_sigusr2.sh
+++ tests/test_sigusr2.sh
@@ -19,9 +19,19 @@ if ! mountpoint -q ${LXCFSDIR}; then
     exit 1
 fi
 
-[ ! -d /sys/fs/cgroup/cpuset ] && exit 0
+echo "==> Setting up memory cgroup in lxcfs_test_proc"
+[ ! -d /sys/fs/cgroup/memory ] && exit 0
 
-# Test cpuinfo
-[ "$(grep "^processor" ${LXCFSDIR}/proc/cpuinfo | wc -l)" = "$(grep "^processor" /proc/cpuinfo | wc -l)" ]
+initmemory=`awk -F: '/memory/ { print $3 }' /proc/1/cgroup`
+mempath=/sys/fs/cgroup/memory/${initmemory}
+rmdir ${mempath}/lxcfs_test_proc 2>/dev/null || true
+mkdir ${mempath}/lxcfs_test_proc
+echo 1 > ${mempath}/lxcfs_test_proc/tasks
+
+echo $((64*1024*1024)) > ${mempath}/lxcfs_test_proc/memory.limit_in_bytes
+
+# Test meminfo
+echo "==> Testing /proc/meminfo"
+[ "$(grep "^MemTotal:.*kB$" ${LXCFSDIR}/proc/meminfo)" = "$(grep "^MemTotal:.*kB$"  /proc/meminfo)" ]
 
 PASS=1
