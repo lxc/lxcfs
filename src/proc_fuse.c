lxc/lxcfs

commit 25982f5d7e008e6ad13df28af3006ab8286f4c97

Set the file size to 4k
In fuse2, the apparant file size is always 4k, however in fuse3
the file size is actually obeyed, so we need to same something
bigger than 0 to get clients to be able to read the file contents.

Signed-off-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>

src/proc_fuse.c
@@ -103,7 +103,7 @@ __lxcfs_fuse_ops int proc_getattr(const char *path, struct stat *sb)
 	    strcmp(path, "/proc/diskstats")	== 0 ||
 	    strcmp(path, "/proc/swaps")		== 0 ||
 	    strcmp(path, "/proc/loadavg")	== 0) {
-		sb->st_size = 0;
+		sb->st_size = 4096;
 		sb->st_mode = S_IFREG | 00444;
 		sb->st_nlink = 1;
 		return 0;

src/sysfs_fuse.c
@@ -178,7 +178,7 @@ __lxcfs_fuse_ops int sys_getattr(const char *path, struct stat *sb)
 	}
 
 	if (strcmp(path, "/sys/devices/system/cpu/online") == 0) {
-		sb->st_size = 0;
+		sb->st_size = 4096;
 		sb->st_mode = S_IFREG | 00444;
 		sb->st_nlink = 1;
 		return 0;

src/cgroup_fuse.c
@@ -615,7 +615,7 @@ __lxcfs_fuse_ops int cg_getattr(const char *path, struct stat *sb)
 		sb->st_nlink = 1;
 		sb->st_uid = k->uid;
 		sb->st_gid = k->gid;
-		sb->st_size = 0;
+		sb->st_size = 4096;
 		free_key(k);
 		if (!caller_is_in_ancestor(initpid, controller, path1, NULL)) {
 			ret = -ENOENT;

commit ce554964ff34b635b037293e62a0f12c654f0953

Determine the file size from the current system file size
fixes: #448 caused by 25982f5d7e008e6ad13df28af3006ab8286f4c97

Signed-off-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>

src/proc_fuse.c
@@ -80,6 +80,23 @@ struct memory_stat {
 	uint64_t total_unevictable;
 };
 
+static off_t get_procfile_size(const char *path)
+{
+	__do_fclose FILE *f = NULL;
+	__do_free char *line = NULL;
+	size_t len = 0;
+	ssize_t sz, answer = 0;
+
+	f = fopen(path, "re");
+	if (!f)
+		return 0;
+
+	while ((sz = getline(&line, &len, f)) != -1)
+		answer += sz;
+
+	return answer;
+}
+
 __lxcfs_fuse_ops int proc_getattr(const char *path, struct stat *sb)
 {
 	struct timespec now;
@@ -104,7 +121,7 @@ __lxcfs_fuse_ops int proc_getattr(const char *path, struct stat *sb)
 	    strcmp(path, "/proc/swaps")		== 0 ||
 	    strcmp(path, "/proc/loadavg")	== 0 ||
 	    strcmp(path, "/proc/slabinfo")	== 0) {
-		sb->st_size = 4096;
+		sb->st_size = get_procfile_size(path);
 		sb->st_mode = S_IFREG | 00444;
 		sb->st_nlink = 1;
 		return 0;
@@ -130,23 +147,6 @@ __lxcfs_fuse_ops int proc_readdir(const char *path, void *buf,
 		return -EINVAL;
 
 	return 0;
-}
-
-static off_t get_procfile_size(const char *path)
-{
-	__do_fclose FILE *f = NULL;
-	__do_free char *line = NULL;
-	size_t len = 0;
-	ssize_t sz, answer = 0;
-
-	f = fopen(path, "re");
-	if (!f)
-		return 0;
-
-	while ((sz = getline(&line, &len, f)) != -1)
-		answer += sz;
-
-	return answer;
 }
 
 __lxcfs_fuse_ops int proc_open(const char *path, struct fuse_file_info *fi)

src/sysfs_fuse.c
@@ -178,7 +178,7 @@ __lxcfs_fuse_ops int sys_getattr(const char *path, struct stat *sb)
 	}
 
 	if (strcmp(path, "/sys/devices/system/cpu/online") == 0) {
-		sb->st_size = 4096;
+		sb->st_size = get_sysfile_size (path);
 		sb->st_mode = S_IFREG | 00444;
 		sb->st_nlink = 1;
 		return 0;