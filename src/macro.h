081d7647093b241dba4b9ced9d1771e032a84ce2
Christian Brauner <christian.brauner@ubuntu.com>
2021-09-02 17:02:24 +0200

    macro: fix bracket warnings in non-debug mode
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- src/macro.h
+++ src/macro.h
@@ -28,10 +28,10 @@
 #define CGROUP2_SUPER_MAGIC 0x63677270
 #endif
 
-#define lxcfs_debug_stream(stream, format, ...)                                \
-	do {                                                                   \
-		fprintf(stream, "%s: %d: %s: " format "\n", __FILE__, __LINE__,     \
-			__func__, ##__VA_ARGS__);                                \
+#define lxcfs_debug_stream(stream, format, ...)                       \
+	do {                                                          \
+		fprintf(stream, "%s: %d: %s: " format "\n", __FILE__, \
+			__LINE__, __func__, ##__VA_ARGS__);           \
 	} while (false)
 
 #define lxcfs_error(format, ...) lxcfs_debug_stream(stderr, format, ##__VA_ARGS__)
@@ -39,7 +39,9 @@
 #ifdef DEBUG
 #define lxcfs_debug(format, ...) lxcfs_error(format, ##__VA_ARGS__)
 #else
-#define lxcfs_debug(format, ...)
+#define lxcfs_debug(format, ...) \
+	do {                     \
+	} while (false)
 #endif /* DEBUG */
 
 #ifdef VERBOSE
