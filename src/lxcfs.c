6abb941e1e42042ca6ea1b8c42bf7e1b66610ed0
Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
2024-10-02 11:08:03 +0200

    DO NOT MERGE IT'S TESTS TESTS :-)
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>

--- src/lxcfs.c
+++ src/lxcfs.c
@@ -880,7 +880,7 @@ static int lxcfs_open(const char *path, struct fuse_file_info *fi)
 
 static int lxcfs_read(const char *path, char *buf, size_t size, off_t offset,
 		      struct fuse_file_info *fi)
-{
+{ 
 	int ret;
 	enum lxcfs_virt_t type;
 	
