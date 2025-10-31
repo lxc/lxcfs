59fca61464523ae406346dc7d1b5d4788023e087
Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
2025-10-31 14:20:23 +0100

    lxcfs: wire up ->write callback for /proc
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>

--- src/proc_fuse.c
+++ src/proc_fuse.c
@@ -1722,3 +1722,9 @@ __lxcfs_fuse_ops int proc_read(const char *path, char *buf, size_t size,
 
 	return -EINVAL;
 }
+
+__lxcfs_fuse_ops int proc_write(const char *path, const char *buf, size_t size,
+				off_t offset, struct fuse_file_info *fi)
+{
+	return -EINVAL;
+}
--- src/proc_fuse.h
+++ src/proc_fuse.h
@@ -20,6 +20,7 @@ __visible extern int proc_readdir(const char *path, void *buf, fuse_fill_dir_t f
 __visible extern int proc_open(const char *path, struct fuse_file_info *fi);
 __visible extern int proc_access(const char *path, int mask);
 __visible extern int proc_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
+__visible extern int proc_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
 __visible extern int proc_release(const char *path, struct fuse_file_info *fi);
 
 #endif /* __LXCFS_PROC_FUSE_H */
